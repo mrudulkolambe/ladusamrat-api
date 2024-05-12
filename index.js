5e2d5512df14bd30da04c43ba7af634f1bc7dd61
mrudulkolambe <mrudulkolambe02@gmail.com>
2024-05-12 18:27:18 +0530

    url changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const axios = require('axios');
 
 const app = express();
 app.use(cors({
-    origin: ["*", "http://localhost:3000", "http://localhost:3001", "https://ladusamrat-store.vercel.app", "https://ladusamrat-admin.vercel.app"]
+    origin: ["*", "http://localhost:3000", "http://localhost:3001", "https://ladusamrat-store.vercel.app", "https://ladusamrat-admin.vercel.app", "https://www.ladusamrat.in"]
 }))
 app.use(bodyParser.json());
 const PORT = process.env.PORT;
